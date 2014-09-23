a3a0ccc094a4beaf794806170509afe74434bcce
almonacilo <registros@almocuba.com>
2014-09-23 18:32:30 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,7 +2,8 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';           
 set_time_limit( 24 * 3600 );
-date_default_timezone_set(date_default_timezone_get());
+echo date_default_timezone_get();
+date_default_timezone_set();
 
 $base_url = "http://www.groupon.es/browse/zaragoza?category=restaurantes";
 $script_tz = date_default_timezone_get();
